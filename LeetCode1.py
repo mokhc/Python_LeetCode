cd0b4fcc5b8725a9c67174d79c77e4107c32baa2
mokhc <mokhonchuen@yahoo.com>
2024-08-29 23:39:01 +0800

    Update LeetCode1.py

--- LeetCode1.py
+++ LeetCode1.py
@@ -1,18 +1,28 @@
-# Test Program - LeetCode 1
+# Test - LC1
+# @Author - mokhc
+# @Date - 28/08/24
 # Trivial - How can you add value to a key in a dict?
-# Runtime ~ 88.90%
 from typing import List
 
 
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        # initialize
-        list = {} # dict
+       # initialize
+        list = {}
         ind = 0
+        back = len(nums) - 1
         # loop to find value in list
         while ind < len(nums):
             if target - nums[ind] in list.keys():
-                return list[target - nums[ind]], ind
+                return sorted((list[target - nums[ind]], ind))
             # add index as value
             list[nums[ind]] = ind
-            ind += 1
+            if target - nums[back] in list.keys():
+                return sorted((list[target - nums[back]], back))
+            # add index as value
+            list[nums[back]] = back
+            # increment
+            ind+=1
+            # decrement
+            back-=1
+
