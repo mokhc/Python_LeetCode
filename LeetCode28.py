9bd47e018002412003e58857cb7eb9d63706d61c
mokhc <mokhonchuen@yahoo.com>
2024-08-30 10:34:08 +0800

    Update and rename LC28.py to LeetCode28.py

---
+++ LeetCode28.py
@@ -0,0 +1,8 @@
+# Test - LC28
+# @Author - mokhc
+# @Date - 29/08/24
+# Trivial - What is meant by comparing two strings?
+
+class Solution1:
+    def strStr(self, haystack: str, needle: str) -> int:
+            return haystack.find(needle)
